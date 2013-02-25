80bff4d1de4992b49e70019c1f4a9563c4159aab
Joshua Deare <doodles@doodles.(none)>
2013-02-24 21:21:46 -0800

    found way to get around x, y for Vector2D issues Needs implementation for 3D still need to update __dir__ to include them closes #2 refs #3

--- vector.py
+++ vector.py
@@ -1,4 +1,4 @@
-class Vector():
+class Vector(object):
     """
     Custom Vector class
 
@@ -13,9 +13,9 @@ class Vector():
         """
         #Might should add a TypeError if there are non number args
         if isinstance(args[0], (tuple, list)):
-            self.position = tuple(args[0])
+            self.position = list(args[0])
         else: 
-            self.position = args
+            self.position = list(args)
 
     def printVector(self):
         """
@@ -30,9 +30,16 @@ class Vector():
     def getPoint(self, index):
         """
         Gets the value in the position tuple of given index
+        Assumes the user will use 1 for the first coor
         """
         return self.position[index-1]
     
+    def setPoint(self, index, value):
+        """
+        Sets an index in position to the given value
+        Assumes the user will use 1 for the first coor
+        """
+        self.position[index - 1] = value
 
 
 
@@ -99,9 +106,65 @@ class Vector():
         """
         return len(self.position)
 
-class DVector(Vector):
+class Vector2D(Vector):
      
     def __init__(self, x, y):
+        """
+        Use Super's constructor and add x, y variables for easy access
+        """
         Vector.__init__(self, x, y)
+
+    #Accessors/Mutators#
+
+    def __getattribute__(self, name):
+        if name == 'x':
+            return object.__getattribute__(self, 'position')[0]
+        elif name == 'y':
+            return object.__getattribute__(self, 'position')[1]
+        else:
+            return object.__getattribute__(self, name)
+
+    def __setattr__(self, name, value):
+        if name == 'x':
+            old = self.position
+            old[0] = value
+            object.__setattr__(self, 'position', old)
+        elif name == 'y':
+            old = self.position
+            old[1] = value
+            object.__setattr__(self, 'position', old)
+        else:
+            object.__setattr__(self, name, value)
+
+class Vector3D(Vector):
+    def __init__(self, x, y, z):
+        Vector.__init__(self, x, y, z)
         self.x = x
         self.y = y
+        self.z = z
+
+    #Accessors and Mutators#
+
+    def getX(self):
+        """
+        returns value of x
+        """
+        return self.x
+
+    def getY(self):
+        """
+        return value of y
+        """
+        return self.y
+    
+    def getZ(self):
+        """
+        return value of z
+        """
+        return self.z
+
+    def setX(self):
+        """
+        sets the value of z
+        """
+        self.set
