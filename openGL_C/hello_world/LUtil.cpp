b24e408dd99483c66c2223006c77f4f247736701
Josh Deare <josh.deare@coreos.com>
2013-07-09 20:20:45 -0700

    made oblong quad

--- openGL_C/hello_world/LUtil.cpp
+++ openGL_C/hello_world/LUtil.cpp
@@ -36,7 +36,7 @@ void render()
 
     //Render quad
     glBegin( GL_QUADS );
-        glVertex2f( -0.5f, -0.5f );
+        glVertex2f( -0.25f, -0.5f );
         glVertex2f(  0.5f, -0.5f );
         glVertex2f(  0.5f,  0.5f );
         glVertex2f( -0.5f,  0.5f );
